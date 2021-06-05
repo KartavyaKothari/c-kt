8903fbc4cde1178dfc731a16599598b2d14fb124
kartavya kothari <kartavya.kothari24@gmail.com>
2021-06-05 09:55:17 +0530

    Added code

---
+++ comments.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    // This is single line comment
+    /*
+        This 
+        is
+        multiline comment
+    */
+   
+
+    return 0;
+}
---
+++ introduce.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    int age = 24;
+    double var = 3.14;
+    // float var = 
+    char c = 'a';
+    char *name = "Kartavya\0";
+
+    printf("Hello my age is %d",age);
+
+    return 0;
+}
